29668bdc7f01e3f534e7d600136d842192a67bc9
Hackintosh 5 <hackintosh5@wildfyre.net>
2021-03-27 13:10:00 +0100

    add async calls for non-async functions
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

---
+++ api/helpers/utils/utils.py
@@ -0,0 +1,8 @@
+import asyncio
+import functools
+
+
+def run_sync(func, *args, **kwargs):
+    """Run a non-async function in a new thread and return an awaitable"""
+    # Returning a coro
+    return asyncio.get_event_loop().run_in_executor(None, functools.partial(func, *args, **kwargs))
