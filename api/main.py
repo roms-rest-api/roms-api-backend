89dc23144a3c60009ffba952603eb35d34bc0697
pqhaa <maybepqhaz@gmail.com>
2021-03-31 21:05:32 +0300

    [WIP]
    Signed-off-by: pqhaa <maybepqhaz@gmail.com>

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 from . import app
-from .routers import main as main_route, changelog, upload_to_gdrive
+from .routers import main as main_route, changelog, upload_to_gdrive, frontend
 
+from fastapi.middleware.cors import CORSMiddleware
 
 def main():
+    app.add_middleware(
+        CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],
+    )
+
     app.include_router(router=main_route.router)
     app.include_router(router=changelog.router)
     app.include_router(router=upload_to_gdrive.router)
+    app.include_router(router=frontend.router)
