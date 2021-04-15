82f29a0443b250d06fef9067df31c9fe1dd212e1
Billaids <36324542+Billaids@users.noreply.github.com>
2021-04-15 20:08:07 +0200

    Wip bill3 (#22)
    * ignore devices.yaml
    
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>
    
    * use devices.yaml from repo
    
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>
    
    * add scheduler for updatin devices.yaml
    
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- api/helpers/configs/devices.py
+++ api/helpers/configs/devices.py
@@ -1,15 +1,31 @@
 import yaml
+import requests
+import os
+
+from loguru import logger
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 class DevicesConfig:
-    def __init__(self, devices_config):
-        self.filename = devices_config
-
+    def __init__(self, devices_url):
+        self.__DEVICES_URL = devices_url
+        self.scheduler = BackgroundScheduler(daemon=True)
         self.init_file()
+        self.update_yaml()
 
     def init_file(self):
-        with open(self.filename, "r") as config_file:
-            self.config = yaml.load(config_file.read(), Loader=yaml.FullLoader)
+        try:
+            response = requests.get(self.__DEVICES_URL)
+            yaml_file = os.path.basename(self.__DEVICES_URL)
+            with open(yaml_file, "wb") as file:
+                file.write(response.content)
+
+            logger.info(f"Successfully download {yaml_file}")
+            with open(yaml_file, "r") as config_file:
+                self.config = yaml.load(config_file.read(), Loader=yaml.FullLoader)
+
+        except Exception as e:  # TODO: Add exceptions
+            logger.error(e)
 
     def get(self, name: str) -> dict:
         return self.config.get(name, None)
@@ -20,3 +36,6 @@ class DevicesConfig:
     def save_file(self):
         with open(self.filename, "w") as config_file:
             config_file.write(yaml.dump(self.config))
+
+    def update_yaml(self):
+        self.scheduler.add_job(lambda: self.init_file(), "interval", seconds=3600)
